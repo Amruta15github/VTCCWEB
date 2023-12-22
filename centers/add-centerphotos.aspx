4650e954acb434e64a5e8c8d85a766cacb9bf56f
Amruta <amrutaajari@gmail.com>
2023-12-22 17:15:38 +0530

    update centerphotos

--- centers/add-centerphotos.aspx
+++ centers/add-centerphotos.aspx
@@ -1,12 +1,29 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/centers/MasterAdmin.master" AutoEventWireup="true" CodeFile="add-centerphotos.aspx.cs" Inherits="centers_add_centerphotos" %>
+﻿<%@ Page Title="Center Photos | VTCC" Language="C#" MasterPageFile="~/centers/MasterAdmin.master" AutoEventWireup="true" CodeFile="add-centerphotos.aspx.cs" Inherits="centers_add_centerphotos" %>
 <%@ MasterType VirtualPath="~/centers/MasterAdmin.master" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+     <script>
+		$(document).ready(function () {
+            $('[id$=gvphotos]').DataTable({
+				columnDefs: [
+					{ orderable: false, targets: [0, 1, 2 ] }
+				],
+				order: [[0, 'desc']]
+			});
+		});
+     </script>
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
      <!-- docupload form start -->
  <%--   <span class="space20"></span>--%>
-    <div class="container"> 
+     <h2 class="pgTitle">Center Photos</h2>
+	<span class="space10"></span>
+	 <div id="editinfo" runat="server">
+  
    <div class="card card-primary">
+	   <div class="card-header">
+				<h3 class="card-title"><%=pgTitle %></h3>
+			</div>
         	<%-- Card Body --%>
 			<div class="card-body">
                 <div class="colorLightBlue">
@@ -16,29 +33,71 @@
         <span class="space10"></span>
                 <h1 class="pageH1 fontRegular">Center Photos</h1>
                 <span class="space20"></span>
-              
+                <div class="form-row">
                 <div class="form-group col-md-6">
 						<label>Center Title:*</label>
-						<asp:TextBox ID="txttitle" runat="server" CssClass="form-control" Width="100%"></asp:TextBox>
+						<asp:TextBox ID="txttitle" runat="server" CssClass="form-control" MaxLength="30"></asp:TextBox>
 					</div>
 
                 <div class="form-group col-md-6">
-						<label>Center Image:</label>
+						<label>Center Image:*</label>
 						<asp:FileUpload ID="fuImage" runat="server" CssClass="form-control-file" />
 						<span class="space10"></span>
-						<%--<%= nwsPhoto %>--%>
+						<%= centerphoto %>
                          <span class="space5"></span>
 					
 					</div>
-                    
-                <asp:Button ID="btnSave" runat="server" CssClass="btn btn-primary" Text="Save"  />
-
-                    
-                </div>
+				 </div>
               
             </div>
+		 </div>
+              	<!-- Button controls starts -->      
+          <asp:Button ID="btnSave" runat="server" CssClass="btn btn-primary" Text="Save" OnClick="btnSave_Click"  />
+		 <asp:Button ID="btnDelete" runat="server" CssClass="btn btn-outline-info" Text="Delete" OnClientClick="return confirm('Are you sure to delete?');" OnClick="btnDelete_Click" />
+		<asp:Button ID="btnCancel" runat="server" CssClass="btn btn-outline-dark" Text="Cancel" OnClick="btnCancel_Click"  />
+		<div class="float_clear"></div>
+		<!-- Button controls ends -->                             
    </div>
-        
-      
+        <div id="viewinfo" runat="server">
+		<a href="add-centerphotos.aspx?action=new" runat="server" class="btn btn-primary btn-md">Add New</a>
+		
+		<span class="space20"></span>
+		<div class="formPanel table-responsive-md">
+			<asp:GridView ID="gvphotos" runat="server" CssClass="table table-striped table-bordered table-hover" GridLines="None" 
+				AutoGenerateColumns="false" OnRowDataBound="gvphotos_RowDataBound">
+				 <HeaderStyle CssClass="thead-dark" />
+				<RowStyle CssClass="" />
+				<AlternatingRowStyle CssClass="alt" />
+				<Columns>
+					 <asp:BoundField DataField="CentPhotoId">
+						<HeaderStyle CssClass="HideCol" />
+						<ItemStyle  CssClass="HideCol"/>
+					</asp:BoundField>
+					
+					 <asp:BoundField DataField="CentPhotoTitle" HeaderText="Photo Title">
+						<ItemStyle Width="20%" />
+					</asp:BoundField>
+
+					<asp:TemplateField>
+						<ItemStyle Width="10%" />
+						<ItemTemplate>
+							<asp:Literal ID="litAnch" runat="server"></asp:Literal>
+						</ItemTemplate>
+					</asp:TemplateField> 
+
+					 <%-- <asp:TemplateField>
+						<ItemStyle Width="10%" />
+						<ItemTemplate>
+							<asp:Literal ID="litAnchphto" runat="server"></asp:Literal>
+						</ItemTemplate>
+					</asp:TemplateField> --%>
+					</Columns>
+				<EmptyDataTemplate>
+					<span class="warning">Its Empty Here... :(</span>
+				</EmptyDataTemplate>
+				<PagerStyle CssClass="" />
+			</asp:GridView>
+			</div>
+	</div>    
 </asp:Content>
 
--- centers/add-centerphotos.aspx.cs
+++ centers/add-centerphotos.aspx.cs
@@ -10,23 +10,49 @@ using System.IO;
 public partial class centers_add_centerphotos : System.Web.UI.Page
 {
     iClass c = new iClass();
-    public string rootPath;
+    public string rootPath, pgTitle, centerphoto;
     protected void Page_Load(object sender, EventArgs e)
     {
         try
         {
-
+            pgTitle = Request.QueryString["action"] == "new" ? "Add Center Photos" : "Edit Center Photos";
             btnSave.Attributes.Add("onclick", " this.disabled = true; this.value='Processing...'; " + ClientScript.GetPostBackEventReference(btnSave, null) + ";");
-           
+            btnDelete.Attributes.Add("onclick", " this.disabled = true; this.value='Processing...'; " + ClientScript.GetPostBackEventReference(btnDelete, null) + ";");
+            btnCancel.Attributes.Add("onclick", " this.disabled = true; this.value='Processing...'; " + ClientScript.GetPostBackEventReference(btnCancel, null) + ";");
+
 
             if (!IsPostBack)
             {
+                if (Request.QueryString["action"] != null)
+                {
+                    editinfo.Visible = true;
+                    viewinfo.Visible = false;
 
-                //GetData(Convert.ToInt32(Session["centerMaster"]));
+                    if (Request.QueryString["action"] == "new")
+                    {
+                        btnSave.Text = "Save Info";
+                        btnDelete.Visible = false;
+                       
+                        //btnRemove.Visible = false;
+                    }
+                    else
+                    {
+                        btnSave.Text = "Modify Info";
+                        btnDelete.Visible = true;
+                        GetData(Convert.ToInt32(Request.QueryString["id"]));
+                    }
+                }
+                else
+                {
+                    editinfo.Visible = false;
+                    viewinfo.Visible = true;
+                    FillGrid();
+                }
 
-
+               
             }
             lblId.Visible = false;
+
         }
         catch (Exception ex)
         {
@@ -37,26 +63,65 @@ public partial class centers_add_centerphotos : System.Web.UI.Page
 
     }
 
-    public void GetData(int centerIdx)
+    private void FillGrid()
     {
         try
         {
-            using (DataTable dtcenter = c.GetDataTable("select * from CentersData where CenterID=" + centerIdx))
+            using (DataTable dtphoto = c.GetDataTable("Select CentPhotoId,  CentPhotoTitle From CenterPhotos Order By CentPhotoId DESC"))
             {
-                if (dtcenter.Rows.Count > 0)
+                gvphotos.DataSource = dtphoto;
+                gvphotos.DataBind();
+
+                if (dtphoto.Rows.Count > 0)
                 {
-                    DataRow row = dtcenter.Rows[0];
-                    lblId.Text = centerIdx.ToString();
+                    gvphotos.UseAccessibleHeader = true;
+                    gvphotos.HeaderRow.TableSection = TableRowSection.TableHeader;
+                }
+            }
 
-                    if (row["CenterLogo"] != DBNull.Value && row["CenterLogo"] != null && row["CenterLogo"].ToString() != "" && row["CenterLogo"].ToString() != "no-photo.png")
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('error', 'Error Occoured While Processing');", true);
+            c.ErrorLogHandler(this.ToString(), "FillGrid", ex.Message.ToString());
+            return;
+        }
+    }
+
+    public void GetAllControls(ControlCollection ctrs)
+    {
+        foreach (Control c in ctrs)
+        {
+            if (c is System.Web.UI.WebControls.TextBox)
+            {
+                TextBox tt = c as TextBox;
+                tt.Text = tt.Text.Trim().Replace("'", "");
+            }
+            if (c.HasControls())
+            {
+                GetAllControls(c.Controls);
+            }
+        }
+    }
+
+    public void GetData(int caractIdx)
+    {
+        try
+        {
+            using (DataTable dtcar = c.GetDataTable("select * from CenterPhotos where CentPhotoId=" + caractIdx))
+            {
+                if (dtcar.Rows.Count > 0)
+                {
+                    DataRow row = dtcar.Rows[0];
+                    lblId.Text = caractIdx.ToString();
+
+                    txttitle.Text = row["CentPhotoTitle"].ToString();
+
+                    if (row["CentPhotoFile"] != DBNull.Value && row["CentPhotoFile"] != null && row["CentPhotoFile"].ToString() != "" && row["CentPhotoFile"].ToString() != "no-photo.png")
                     {
-
-                        centerlogo = "<a href=\"" + Master.rootPath + "upload/centerlogo/" + row["CenterLogo"].ToString() + "\"width=\"200\" \" target=\"_blank\"> View logo </a>";
-
+                        centerphoto = "<img src=\"" + Master.rootPath + "upload/centerphotos/" + row["CentPhotoFile"].ToString() + "\" width=\"200\" />";
 
                     }
-
-                   
 
                 }
             }
@@ -67,5 +132,131 @@ public partial class centers_add_centerphotos : System.Web.UI.Page
             c.ErrorLogHandler(this.ToString(), "GetData", ex.Message.ToString());
             return;
         }
+    }
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        try
+        {
+
+            GetAllControls(this.Controls);
+            //Empty Validations
+            if (txttitle.Text == "" )
+            {
+                ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('warning', 'All * Fields are mandatory');", true);
+                return;
+            }
+            //Insert Update data
+            int maxId = lblId.Text == "[New]" ? c.NextId("CenterPhotos", "CentPhotoId") : Convert.ToInt32(lblId.Text);
+
+            if (Session["centerMaster"] != null)
+            {
+                int centerID = Convert.ToInt32(Session["centerMaster"]);
+
+                string centerphoto = "";
+                if (fuImage.HasFile)
+                {
+                    string fExt = Path.GetExtension(fuImage.FileName).ToString().ToLower();
+
+                    if (fExt == ".jpeg" || fExt == ".png" || fExt == ".pdf" || fExt == ".jpg")
+                    {
+                        centerphoto = "center-photo-" + Session["centerMaster"].ToString() + DateTime.Now.ToString("ddMMyyyyHHmmss") + fExt;
+                        fuImage.SaveAs(Server.MapPath("~/upload/centerphotos/") + centerphoto);
+
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('warning', 'Only  .jpeg .png .pdf .jpg files are allowed');", true);
+                        return;
+
+                    }
+
+                }
+                else
+                {
+                    if (lblId.Text == "[New]")
+                    {
+                        ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('warning', 'Select Photos to upload');", true);
+                        return;
+                    }
+                }
+
+
+
+                if (lblId.Text == "[New]")
+                {
+                    c.ExecuteQuery("Insert into CenterPhotos(CentPhotoId, FK_CenterID, CentPhotoTitle, CentPhotoFile ) Values (" + maxId + ",'" + Session["centerMaster"] + "', '" + txttitle.Text + "', '" + centerphoto + "')");
+                   
+
+                    ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('success', 'Center Photo Added');", true);
+                }
+
+                else
+                {
+                    c.ExecuteQuery("Update CenterPhotos set  CentPhotoTitle='" + txttitle.Text + "', CentPhotoFile='" + centerphoto+ "' where CentPhotoId=" + maxId);
+
+                   
+
+                    ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('success', 'Center Photo  Updated');", true);
+                }
+                ScriptManager.RegisterClientScriptBlock(this, GetType(), "CallMyFunction", "waitAndMove('add-centerphotos.aspx', 2000);", true);
+
+                txttitle.Text = ""; //clear
+
+
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('error', 'Error Occoured While Processing');", true);
+            c.ErrorLogHandler(this.ToString(), "btnSave_Click", ex.Message.ToString());
+            return;
+        }
+    }
+
+
+
+    protected void gvphotos_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        try
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                Literal litAnch = (Literal)e.Row.FindControl("litAnch");
+                litAnch.Text = "<a href=\"add-centerphotos.aspx?action=edit&id=" + e.Row.Cells[0].Text + "\"class=\"gAnch\" title=\"View/Edit\"></a>";
+
+
+                //Literal litAnchphto = new Literal();
+                //litAnchphto = (Literal)e.Row.FindControl("litAnchphto");
+                //litAnchphto.Text = "<a href=\"add-centerphotos.aspx?albumId=" + e.Row.Cells[0].Text + "\" class=\"addPhoto\" title=\"Add Photos \"></a>";
+
+            }
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('error', 'Error Occoured While Processing');", true);
+            c.ErrorLogHandler(this.ToString(), "gvcareer_RowDataBound", ex.Message.ToString());
+            return;
+        }
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            c.ExecuteQuery("Delete CenterPhotos where CentPhotoId=" + Request.QueryString["id"]);
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('success', 'Center Photos Deleted');", true);
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "CallMyFunction", "waitAndMove('add-centerphotos.aspx', 2000);", true);
+        }
+        catch (Exception ex)
+        {
+            ScriptManager.RegisterClientScriptBlock(this, GetType(), "myScript", "TostTrigger('error', 'Error Occoured While Processing');", true);
+            c.ErrorLogHandler(this.ToString(), "btnDelete_Click", ex.Message.ToString());
+            return;
+        }
+    }
+
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("add-centerphotos.aspx");
     }
 }
